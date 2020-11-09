62ccfcf816a37f76f6ff1c429978e4584601e49b
saravanan-1990 <saravanan.s@go-mmt.com>
2020-11-09 17:32:31 +0530

    add test case for the features done

--- test/controllers/companies_controller_test.rb
+++ test/controllers/companies_controller_test.rb
@@ -47,7 +47,7 @@ class CompaniesControllerTest < ApplicationSystemTestCase
       fill_in("company_name", with: "New Test Company")
       fill_in("company_zip_code", with: "28173")
       fill_in("company_phone", with: "5553335555")
-      fill_in("company_email", with: "new_test_company@test.com")
+      fill_in("company_email", with: "new_test_company@getmainstreet.com")
       click_button "Create Company"
     end
 
@@ -56,6 +56,17 @@ class CompaniesControllerTest < ApplicationSystemTestCase
     last_company = Company.last
     assert_equal "New Test Company", last_company.name
     assert_equal "28173", last_company.zip_code
+    assert_match( /\b[A-Z0-9._%a-z\-]+@getmainstreet\.com\z/, last_company.email, ["must be a getmainstreet.com account"] ) unless last_company.email.blank?
+  end
+
+  test "Destroy" do
+    companies_count = Company.count
+    visit company_path(@company)
+    accept_confirm do
+      click_link "Delete"
+    end
+    assert_text "Deleted Successfully"
+    assert_equal (companies_count - 1), Company.count
   end
 
 end
